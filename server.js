679d6e8a322bdd971a79ecaf6b4ce6cb2a2bc2b6
Steve Golden <sgolden2525@outlook.com>
2019-04-08 12:52:53 -0500

    Create new user
    Added shortid node package.
    Use the shortid for the _id for the id.
    
    Added the schema for the site
    Added the model for the site
    
    Added the post route and create new users and id.
    
    Added the response to show the userId

--- server.js
+++ server.js
@@ -1,23 +1,80 @@
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
-
 const cors = require('cors')
-
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track', { useNewUrlParser: true })
+const shortid = require('shortid')
+
+
+const app = express()
+
+mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost:27017/ExerciseTracker', { useNewUrlParser: true }, (err) => {
+  if (!err) {
+    console.log("Database connection successful")
+  } else {
+    console.log("Database is not connected: " + err)
+  }
+})
 
 app.use(cors())
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-app.use(express.static('public'))
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+//Schema
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  _id: {
+    'type': String,
+    'default': shortid.generate
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  exercise: [{
+    description: {
+      type: String,
+      required: true
+    },
+    duration: {
+      type: Number,
+      required: true
+    },
+    date: Date
+  }]
+})
+
+//Database Model                 (collection Name, Schema Name)
+var exerciseUser = mongoose.model('exerciseUsers', userSchema);
+
+//
+app.post('/api/exercise/new-user', (req, res) => {
+
+
+  var eUser = new exerciseUser({ username: req.body.username })
+
+  eUser.save(eUser, function (err, user) {
+  console.log("User when saving is: " + user._id);
+    if (err) { return console.error(err) }
+    else{
+      res.send('Your userId to access the tracker is: ' + user._id);
+    }
+   
+  })
+ 
+  
+  
+})
 
 // Not found middleware
 app.use((req, res, next) => {
@@ -42,6 +99,8 @@ app.use((err, req, res, next) => {
   res.status(errCode).type('txt')
     .send(errMessage)
 })
+
+
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
